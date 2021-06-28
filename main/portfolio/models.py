14bbbb140e5a742562940df4b7ee08b6ce121638
Célestin <intelce.developpement@gmail.com>
2021-06-28 15:40:12 +0200

    Add Projects model

--- main/portfolio/models.py
+++ main/portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='projects-img')
+    github_link = models.URLField(max_length=500)
+    site_link = models.URLField(max_length=500, blank=True)
