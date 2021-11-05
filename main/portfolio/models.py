dd1dd6869dc1f2d1fb60c88a756bc281e87abd46
Instelce <intelce.developpement@gmail.com>
2021-11-05 14:38:58 +0100

    Remove repositorie

--- main/portfolio/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Projects(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to='projects-img')
-    github_link = models.URLField(max_length=500)
-    site_link = models.URLField(max_length=500, blank=True)
