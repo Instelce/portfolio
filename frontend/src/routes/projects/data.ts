2fb2b38a21ad5ca23599ed86c0a22269ca748be1
Instelce <instelce@protonmail.com>
2024-06-17 16:00:25 +0200

    add some projects

--- frontend/src/routes/projects/data.ts
+++ frontend/src/routes/projects/data.ts
@@ -20,21 +20,39 @@ export const projects: ProjectType[] = [
     palette: ['#1F1F1F', '#1F1F1F', '#393939', '#393939', '#9DC16E']
   },
   {
-    id: 4,
-    slug: 'portfolio',
-    name: 'Ce portfolio',
-    description: 'Le portfolio sur lequel vous naviguez',
+    id: 2,
+    slug: 'sudoku',
+    name: 'Sudoku',
+    description: 'Le jeu du sudoku',
     large_description: '',
-    year: 2024,
-    main_image: '/images/projects/portfolio/main.png',
-    images: [],
-    github_link: 'https://github.com/Instelce/portfolio',
-    skills: [1, 2, 3, 4],
-    tecks: ['svelte', 'typescript', 'sass', 'vite', 'threejs'],
-    palette: ['#0D0D0D', '#2A2A2A', '#1E1D46', '#1E1D46', '#5856D6']
+    year: 2023,
+    main_image: '/images/projects/sudoku-c/main.png',
+    images: [
+      '/images/projects/sudoku-c/1.png'
+    ],
+    github_link: 'https://github.com/Instelce/sudoku-c',
+    skills: [1],
+    tecks: ['c'],
+    palette: ['#ffffff', '#c1c1c1', '#6c6c6c', '#323232', '#000000']
   },
   {
-    id: 2,
+    id: 3,
+    slug: 'sudoku-solver',
+    name: 'Sudoku résolveur',
+    description: 'Un résolveur de sudoku',
+    large_description: '',
+    year: 2023,
+    main_image: '/images/projects/sudoku-solver/main.png',
+    images: [
+      '/images/projects/sudoku-solver/1.png'
+    ],
+    github_link: 'https://github.com/Instelce/sudoku-solver',
+    skills: [2, 6],
+    tecks: ['c'],
+    palette: ['#ffffff', '#c1c1c1', '#6c6c6c', '#323232', '#000000']
+  },
+  {
+    id: 4,
     slug: 'doc-barbu',
     name: 'doc-barbu',
     description: 'Un générateur de documentation',
@@ -45,12 +63,12 @@ export const projects: ProjectType[] = [
       '/images/projects/doc-barbu/1.png'
     ],
     github_link: 'https://github.com/Instelce/doc-barbu',
-    skills: [1, 3, 5, 6],
+    skills: [3],
     tecks: ['bash', 'docker', 'php'],
     palette: ['#3f3f46', '#3f3f46', '#3b82f6', '#3b82f6', '#18181b']
   },
   {
-    id: 3,
+    id: 5,
     slug: 'tabas-kho',
     name: 'Tabas-KHO',
     description: 'Un site de streaming fictif',
@@ -65,7 +83,25 @@ export const projects: ProjectType[] = [
     palette: ['#cd8ffa', '#0f0f0f', '#43403d', '#f67960', '#0f0f0f']
   },
   {
-    id: 5,
+    id: 6,
+    slug: 'le-patio',
+    name: 'Application Le Patio',
+    description: 'Réalisation de l\'application Le Patio',
+    large_description: '',
+    year: 2024,
+    main_image: '/images/projects/gestion-patio/main.png',
+    images: [
+      '/images/projects/gestion-patio/1.png',
+      '/images/projects/gestion-patio/2.png',
+      '/images/projects/gestion-patio/3.png'
+    ],
+    github_link: 'https://github.com/Instelce/gestion-patio',
+    skills: [1, 4, 5, 6],
+    tecks: ['java'],
+    palette: ['#ffe38a', '#c1c1c1', '#6c6c6c', '#323232', '#000000']
+  },
+  {
+    id: 7,
     slug: 'game-of-life',
     name: 'Jeu de la vie en Rust',
     description: "Implémentation du jeu de la vie de Conway en Rust avec Bevy et WebAssembly",
@@ -77,6 +113,20 @@ export const projects: ProjectType[] = [
     skills: [1],
     tecks: ['rust', 'wasm'],
     palette: ['#cd8ffa', '#0f0f0f', '#43403d', '#f67960', '#0f0f0f']
+  },
+  {
+    id: 8,
+    slug: 'portfolio',
+    name: 'Ce portfolio',
+    description: 'Le portfolio sur lequel vous naviguez',
+    large_description: '',
+    year: 2024,
+    main_image: '/images/projects/portfolio/main.png',
+    images: [],
+    github_link: 'https://github.com/Instelce/portfolio',
+    skills: [1, 2, 3, 4],
+    tecks: ['svelte', 'typescript', 'sass', 'vite', 'threejs'],
+    palette: ['#0D0D0D', '#2A2A2A', '#1E1D46', '#1E1D46', '#5856D6']
   },
   // {
   //   id: 5,
